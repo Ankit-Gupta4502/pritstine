a895018411f594366851f4c27887db8d413277c6
Ankit Gupta <ankit@hexabells.com>
2024-03-21 16:57:59 +0530

    fixed height

--- src/components/Layout/EmailNotify.jsx
+++ src/components/Layout/EmailNotify.jsx
@@ -63,73 +63,80 @@ const EmailNotify = () => {
     setShowInput(true);
   };
   return (
-    <div
-      className={`email-wrapper  absolute group cursor-pointer  origin-top-left  duration-500  transition-all  flex items-center top-[calc(62vh+35px)]   overflow-hidden  left-10  ease-linear  z-30 rounded-[30px] bg-primary   h-[56px]  `}
-      ref={input}
-    >
-      <div className="flex px-[10px]   flex-shrink-0 items-center">
-        <div className="bg-white grid flex-shrink-0 place-items-center w-9 h-9 rounded-full">
-          {isSuccess ? (
-            <IoMdCheckmark size={20} />
-          ) : (
-            <LuMail
-              className="group-hover:[transform:rotateY(180deg)] duration-500 transition-all  [transform-style:preserve_3d] [perspective:1000px] "
-              size={20}
+    <div className=" absolute z-30 top-[60vh] left-10">
+      <span className="text-2xl   text-white  mb-5  font-semibold block ">
+        Stay tuned
+      </span>
+      <div
+        className={`email-wrapper   group cursor-pointer  origin-top-left  duration-500  transition-all  flex items-center overflow-hidden       ease-linear   rounded-[30px] bg-primary   h-[56px]  w-fit  `}
+        ref={input}
+      >
+        <div className="flex px-[10px]   flex-shrink-0 items-center">
+          <div className="bg-white grid flex-shrink-0 place-items-center w-9 h-9 rounded-full">
+            {isSuccess ? (
+              <IoMdCheckmark size={20} />
+            ) : (
+              <LuMail
+                className="group-hover:[transform:rotateY(180deg)] duration-500 transition-all  [transform-style:preserve_3d] [perspective:1000px] "
+                size={20}
+              />
+            )}
+          </div>
+
+           <div
+            className={` ${
+              showMssg ? " max-w-[300px] ml-3 " : "max-w-[0px]"
+            } overflow-hidden text-white transition-all whitespace-nowrap duration-300 ease-linear`}
+          >
+            Thank you we will Notify you soon
+          </div>
+
+          {!isSuccess && (
+            <input
+              type="text"
+              placeholder="Enter your Email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                if (error) {
+                  setErrors("");
+                }
+              }}
+              className={`${
+                showInput
+                  ? "  max-w-[220px] border-l-white ml-3  border-l px-2 "
+                  : " max-w-[0px] border-l-primary"
+              } duration-300  origin-center   caret-white placeholder:text-white   transition-all will-change-[max-width] ease-linear   overflow-hidden   bg-transparent focus:outline-0  ${
+                error ? " text-red-600 " : "text-white"
+              }`}
             />
           )}
         </div>
 
-        <div
-          className={` ${
-            showMssg ? " max-w-[300px] ml-3 " : "max-w-[0px]"
-          } overflow-hidden text-white transition-all whitespace-nowrap duration-300 ease-linear`}
+        <button
+          disabled={isPending}
+          className={` disabled:text-primary/40  rounded-tr-[30px] rounded-br-[30px] transition-all whitespace-nowrap overflow-hidden duration-500 ease-linear  flex items-center py-4  ${
+            showInput ? " bg-white text-primary" : "text-white"
+          }  ${
+            isSuccess ? " max-w-[0px] " : "max-w-[124px] px-[10px]"
+          }  overflow-hidden`}
+          onClick={() => {
+            console.log("workingggg");
+            if (!showInput) {
+              handleToggle();
+            }
+            if (showInput) {
+              mutate();
+            }
+          }}
         >
-          Thank you we will Notify you soon
-        </div>
-
-        {!isSuccess && (
-          <input
-            type="text"
-            placeholder="Enter your Email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-              if (error) {
-                setErrors("");
-              }
-            }}
-            className={`${
-              showInput
-                ? "  max-w-[220px] border-l-white ml-3  border-l px-2 "
-                : " max-w-[0px] border-l-primary"
-            } duration-300  origin-center   caret-white placeholder:text-white   transition-all will-change-[max-width] ease-linear   overflow-hidden   bg-transparent focus:outline-0  ${error?" text-red-600 ":"text-white"}`}
+          <span>Notify Me</span>
+          <FiChevronRight
+            className=" ms-2 group-hover:translate-x-1 duration-200 transition-all"
+            size={18}
           />
-        )}
+        </button>
       </div>
-
-      <button
-        disabled={isPending}
-        className={` disabled:text-primary/40  rounded-tr-[30px] rounded-br-[30px] transition-all whitespace-nowrap duration-500 ease-linear  flex items-center py-4  ${
-          showInput ? " bg-white text-primary" : "text-white"
-        }  ${
-          isSuccess ? " max-w-[0px] " : "max-w-[124px] px-[10px]"
-        }  overflow-hidden`}
-        onClick={() => {
-          console.log("workingggg");
-          if (!showInput) {
-            handleToggle();
-          }
-          if (showInput) {
-            mutate();
-          }
-        }}
-      >
-        <span>Notify Me</span>
-        <FiChevronRight
-          className=" ms-2 group-hover:translate-x-1 duration-200 transition-all"
-          size={18}
-        />
-      </button>
     </div>
   );
 };
