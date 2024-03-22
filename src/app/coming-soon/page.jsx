888a8a517eba5873b36ada94a9699873d20b530d
Ankit Gupta <ankit@hexabells.com>
2024-03-22 13:47:59 +0530

    added video media query

--- src/app/coming-soon/page.jsx
+++ src/app/coming-soon/page.jsx
@@ -4,12 +4,29 @@ import React from "react";
 const ComingSoon = () => {
   return (
     <div className=" relative min-h-[calc(100vh-80px)]">
-    
-      <EmailNotify/>
-      <video playsInline controls={false}  className=" absolute w-full h-full inset-0 object-cover" src="/assets/coming_soon.mp4" loop muted autoPlay preload="true"></video>
+      <EmailNotify />
+      <video
+        playsInline
+        controls={false}
+        className=" absolute w-full h-full inset-0 object-cover"
+        loop
+        muted
+        autoPlay
+        preload="true"
+      >
+        {/* desktop */}
+        <source src="/assets/coming_soon.mp4" type="video/mp4" />
+
+        {/* mobile video */}
+        <source
+      
+          src="/assets/coming_soon.mp4"
+          type="video/mp4"
+          media="(max-width:833px)"
+        />
+      </video>
     </div>
   );
 };
 
 export default ComingSoon;
-
